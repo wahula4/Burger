99c0972eaeba99d953353747d8cb9d70026d64cf
wahula4 <mawahula4@gmail.com>
2017-09-08 21:25:38 -0600

    working but not css

--- config/connection.js
+++ config/connection.js
@@ -1,1 +1,21 @@
+// Set up MySQL connection.
+var mysql = require("mysql");
 
+var connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "burgers_db"
+});
+
+// Make connection.
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
+
+// Export connection for our DAO to use.
+module.exports = connection;
--- config/orm.js
+++ config/orm.js
@@ -1,1 +1,27 @@
+var connection = require('./connection.js')
 
+var orm = {
+  all: function(tableInput, callback){
+    connection.query('SELECT * FROM ' + tableInput + ';', function(err, result){
+      if(err) throw err;
+      callback(result)
+    })
+  },
+
+  update: function(tableInput, condition, callback){
+    connection.query('UPDATE ' + tableInput + ' SET devoured=true WHERE id='
+    + condition + ';', function(err,result){
+      if(err) throw err;
+      callback(result);
+    })
+  },
+
+  create: function(tableInput,val,callback){
+    connection.query('INSERT INTO ' + tableInput + " (burger_name) VALUES (' "+ val +" ');", function(err,result){
+      if(err) throw err;
+      callback(result);
+    })
+  }
+}
+
+module.exports = orm;
