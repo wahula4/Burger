86b522a970dc013db03519a77e9c4fadad0a9a9c
wahula4 <mawahula4@gmail.com>
2017-09-10 17:11:03 -0600

    not working on heroku

--- config/connection.js
+++ config/connection.js
@@ -4,7 +4,7 @@ var mysql = require("mysql");
 var connection = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "",
+  password: "Wolfpack4",
   database: "burgers_db"
 });
 
